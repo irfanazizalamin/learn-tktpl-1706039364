5e3730d9400b9de88da7115dfcd6ec473d5219f8
irfan.aziz <irfan.aziz@mamiteam.com>
2020-12-14 22:24:10 +0700

    Done lab 7

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/irfanazizalamin/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/irfanazizalamin/helloworld/MainActivity.kt
@@ -2,10 +2,21 @@ package id.ac.ui.cs.mobileprogramming.irfanazizalamin.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count = 0
+        var counterText: TextView = findViewById(R.id.counterText)
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            count += 1
+            counterText.setText(count.toString());
+        }
     }
 }
